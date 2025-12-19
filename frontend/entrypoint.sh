fd08c44e101952ae3f0b6b412454af70343eda6e
Glays <dansing_wolf@rambler.ru>
2025-12-19 18:32:04 +0300

    Make version.txt readable by all with chmod 644, remove caddy root user

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,6 +2,7 @@
 # Create version file
 echo "COMMIT=$COMMIT"
 echo $COMMIT > /usr/share/nginx/html/version.txt
+chmod 644 /usr/share/nginx/html/version.txt
 # Copy dist to volume, overwriting
 cp -r /app/dist/* /usr/share/nginx/html/
 # Start nginx
