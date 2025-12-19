2ea3c7fc01b1c2e5b810ac08c07fd636d61f0bbb
Glays <dansing_wolf@rambler.ru>
2025-12-19 18:10:26 +0300

    Add debug logging in entrypoint and logs check

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Create version file
+echo "COMMIT=$COMMIT"
 echo $COMMIT > /usr/share/nginx/html/version.txt
 # Copy dist to volume, overwriting
 cp -r /app/dist/* /usr/share/nginx/html/
